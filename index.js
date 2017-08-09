3f869e2e6129da01b0b17751125f7c065ed2c69a
samuelthomps0n <sam@samuelthompson.co.uk>
2017-08-09 11:12:34 +0100

    Allow globbing of files
    Add globbing ability to the source path so that multiple files can be converted in one hit

--- index.js
+++ index.js
@@ -1,60 +1,71 @@
 const Q          = require('q');
 const path       = require('path');
 const fs         = require('fs');
-const lineReader = require("readline");
+const lineReader = require('readline');
+const glob       = require('glob');
 
 module.exports = {
 
     convert: function(src, dest) {
         let deferred = Q.defer();
 
-        let sourceCssStream = lineReader.createInterface({
-            input: fs.createReadStream(src)
-        })
+            let sourceCssStream = lineReader.createInterface({
+                input: fs.createReadStream(src)
+            })
 
-        let rebuiltFile = '';
+            let rebuiltFile = '';
 
-        sourceCssStream.on('line', function(line) {
-            let currentLineWords = line.split(' ');
-            let rebuiltLine = '';
+            sourceCssStream.on('line', function(line) {
+                let currentLineWords = line.split(' ');
+                let rebuiltLine = '';
 
-            currentLineWords.forEach(function(value, index) {
+                currentLineWords.forEach(function(value, index) {
 
-                if(value.startsWith('$')) {
-                    value = 'var(--' + value.replace('$', '') + ')';
-                    if(value.includes(';')) {
-                        value = value.replace(';', '');
-                        value = value + ';';
+                    if(value.startsWith('$')) {
+                        value = 'var(--' + value.replace('$', '') + ')';
+                        if(value.includes(';')) {
+                            value = value.replace(';', '');
+                            value = value + ';';
+                        }
                     }
-                }
 
-                if(index > 0) {
-                    return rebuiltLine += ' ' + value;
-                }
-                return rebuiltLine += value;
+                    if(index > 0) {
+                        return rebuiltLine += ' ' + value;
+                    }
+                    return rebuiltLine += value;
+
+                });
+
+                rebuiltLine = rebuiltLine.replace('    ', '\t') + '\n';
+                rebuiltFile += rebuiltLine;
 
             });
 
-            rebuiltLine = rebuiltLine.replace('    ', '\t') + '\n';
-            rebuiltFile += rebuiltLine;
+            sourceCssStream.on('close', function(line) {
+                if(dest) {
+                    let outputFile = fs.createWriteStream(dest);
 
-        });
+                    outputFile.once('open', function(fd) {
+                        outputFile.write(rebuiltFile);
+                        outputFile.end();
+                    });
+                    outputFile.on('close', function() {
+                        deferred.resolve(dest);
+                    });
+                } else {
+                    let outputFile = fs.createWriteStream(src);
 
-        sourceCssStream.on('close', function(line) {
-            if(dest) {
-                let outputFile = fs.createWriteStream(dest);
+                    outputFile.once('open', function(fd) {
+                        outputFile.write(rebuiltFile);
+                        outputFile.end();
+                    });
+                    outputFile.on('close', function() {
+                        deferred.resolve(src);
+                    });
+                }
+            });
 
-                outputFile.once('open', function(fd) {
-                    outputFile.write(rebuiltFile);
-                    outputFile.end();
-                });
-                outputFile.on('close', function() {
-                    deferred.resolve(dest);
-                });
-            }
-        });
 
         return deferred.promise;
     }
-
 }
